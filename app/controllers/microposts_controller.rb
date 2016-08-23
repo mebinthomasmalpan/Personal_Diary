fd1cfe3a924a53de3d3227badacd37b11ab93845
sameesh-s <sameesh.fisat@gmail.com>
2016-08-24 01:15:59 +0530

    Entry deletion

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,8 @@
 class MicropostsController < ApplicationController
+  def destroy
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to current_user
+  end
 end
